ac5f2cf7501c1099fe4492bc5192f5d68f2692fc
devguiamf <guilherme.amf18@gmail.com>
2023-07-17 15:08:12 -0300

    feat: infra

---
+++ src/domain/repository/product.repository.interface.ts
@@ -0,0 +1,6 @@
+import Product from "../entity/peoduct";
+import RepositoryInterface from "./repository.interface";
+
+export default interface ProductRepositoryInterface extends RepositoryInterface<Product>{
+
+}
---
+++ src/domain/service/order.service.ts
@@ -0,0 +1,21 @@
+import Customer from "../../domain/entity/costomer";
+import Order from "../../domain/entity/order";
+import OrderItem from "../../domain/entity/order_item";
+import { v4 as uuid} from "uuid"
+
+export default class OrderService {
+
+    static placeOrder(custumer: Customer, items: OrderItem[]): Order {
+        if(items.length === 0){Order
+            throw new Error("Order must have at least one item")
+        }
+
+        const order1 = new Order(uuid(), custumer.id, items)
+        custumer.addRewardPoints(order1.total() / 2)
+        return order1
+    }
+    
+    static total(orders: Order[]): number {
+        return orders.reduce((acc,order) => acc + order.total(), 0)
+    }
+}
